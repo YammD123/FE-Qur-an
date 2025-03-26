04f07c9a45fc20710efcfc0a873fab2632027e5e
YammD123 <siamqueen52@gmail.com>
2025-03-26 12:40:36 +0700

    fix inject

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
 	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	import '../app.css';
 	import Navbar from '../components/Navbar.svelte';
+
+	injectAnalytics()
 </script>
 
 <nav>
 	<Navbar />
-	<slot></slot>
+	<slot />
 </nav>
