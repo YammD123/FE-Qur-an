74a3e2e2606e50f356164a6c069991591431afce
YammD123 <siamqueen52@gmail.com>
2025-03-27 21:24:32 +0700

    favicon fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 <svelte:head>
 	<title> Al quran - Baca Al Quran Seacara Online</title>
 	<meta name="description" content="Baca Al-Qur'an online lengkap dengan terjemahan, tajwid, dan audio. Nikmati kemudahan membaca dan memahami ayat-ayat suci Al-Qur'an di mana saja dan kapan saja." />
-
+	<link rel="icon" type="image/png" href="favicon.png" />
 	<meta name="keywords" content="Al-Qur'an online, baca Al-Qur'an, terjemahan Al-Qur'an, tafsir Al-Qur'an, Quran digital, mushaf online, ayat Al-Qur'an, Quran dengan audio, Al-Qur'an dengan tajwid, Quran bahasa Indonesia" />
 
 	<meta property="og:title" content="Baca Al-Qur'an Online | Quran Digital dengan Terjemahan & Tajwid" />
